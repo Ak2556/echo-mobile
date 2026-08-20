c3ce2a3fd39f78c020f60603dbe69a347e911350
Ak2556 <at3236129@gmail.com>
2026-08-20 18:24:09 +0530

    fix(db): disable jsi for SQLiteAdapter to prevent null reference crash in Expo

--- src/shared/database/index.ts
+++ src/shared/database/index.ts
@@ -8,7 +8,7 @@ import Message from './models/Message'
 const adapter = new SQLiteAdapter({
   schema,
   // (You might want to set up migrations here in the future)
-  jsi: true, // fast sync
+  jsi: false, // fast sync
   onSetUpError: error => {
     console.error('Database setup failed', error)
   }
